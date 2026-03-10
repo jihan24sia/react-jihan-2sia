8a3193687f4561d62588643b60e650f67ed3e65d
jihan24sia <jihan24si@mahasiswa.pcr.ac.id>
2026-03-10 09:25:05 +0700

    pertemuan ke 2

---
+++ src/pertemuan-2/HelloWorld.jsx
@@ -0,0 +1,54 @@
+export default function HelloWorld(){
+     const propsUserCard = {
+        nama: "Goku",
+        nim: "999999",
+        tanggal: "2025-01-01"
+    }
+
+    return (
+        <div>
+            
+            
+            <h1>Hello World</h1>
+            <img src="img/mky.png" alt="logo"/>
+            <p>Selamat Belajar ReactJs</p>
+
+            <GreetingBinjai/>
+            <QuoteText/>
+            <UserCard
+            nama="Jihan"
+            nim="2457301071"
+            tanggal="2026/03/10"
+            />
+            <UserCard{...propsUserCard}/>
+            
+        </div>
+    )
+}
+
+function QuoteText() {
+    const text = "Mulutmu Harimaumu";
+    const text2 = "Aku ingin jadi macan";
+    return (
+        <div className="card">
+            <hr/>
+            <p>{text.toLowerCase()}</p>
+            <p>{text2.toUpperCase()}</p>
+        </div>
+    )
+}
+function GreetingBinjai(){
+    return (
+        <small className="card">Salam dari Binjai👌🙌</small>
+    )
+}
+function UserCard(props){
+    return (
+        <div className="card">
+            <hr/>
+            <h3>Nama: {props.nama}</h3>
+            <p>NIM: {props.nim}</p>
+            <p>Tanggal: {props.tanggal}</p>
+        </div>
+    )
+}
