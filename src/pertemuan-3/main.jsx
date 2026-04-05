b5e76b6b7ad99e430c9a279554dd1852ad080110
guest-persil <jihan24si@mahasiswa.pcr.ac.id>
2026-04-05 09:15:53 +0700

    membuat form input konser

--- src/pertemuan-3/main.jsx
+++ src/pertemuan-3/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById("root"))
            
                 {/* <TailwindCSS/>
                  <UserForm/> */}
-                <HitungGajiForm/>
+                <TailwindCSS/>
         </div>
     )
     
