23dfd2e88ce9b49964a53ac874ab67eeac315ca4
guest-persil <jihan24si@mahasiswa.pcr.ac.id>
2026-04-12 14:09:37 +0700

    test

--- src/pertemuan-4/ResponsiveGrid.jsx
+++ src/pertemuan-4/ResponsiveGrid.jsx
@@ -11,7 +11,7 @@ export default function ResponsiveGrid() {
 function ResponsiveText() {
     return (
         <p className="text-sm md:text-lg lg:text-xl xl:text-2xl mb-4">
-            Coba lakukan zoom in atau zoom out. Perhatikan bahwa ukuran teks akan menyesuaikan dengan ukuran layar.<br />
+            Coba lakukan di zoom in atau zoom out. Perhatikan bahwa ukuran teks akan menyesuaikan dengan ukuran layar.<br />
             Coba hapus class yang menggunakan prefix breakpoint (md:xxx, lg:xxx, xl:xxx) dan lihat perbedaannya!
         </p>
     )
