82d3ae40c05140510d7a0af0e669a2c55b47aa6e
jihan24sia <jihan24si@mahasiswa.pcr.ac.id>
2026-04-28 09:45:34 +0700

    pertemuan7 login

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,60 @@
 import "./assets/tailwind.css";
 import { Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
-import Orders from './pages/Orders';
-import Customers from './pages/Customers';
-import NotFound from "./pages/NotFound";
-import Dashboard from "./pages/Dashboard";
-import Error400 from "./pages/Error400";
-import Error401 from "./pages/Error401";
-import Error403 from "./pages/Error403";
-import MainLayout from './layouts/MainLayout';
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import Forgot from "./pages/auth/Forgot";  
-import AuthLayout from "./layouts/AuthLayout";
+// import Orders from './pages/Orders';
+// import Customers from './pages/Customers';
+// import NotFound from "./pages/NotFound";
+// import Dashboard from "./pages/Dashboard";
+// import Error400 from "./pages/Error400";
+// import Error401 from "./pages/Error401";
+// import Error403 from "./pages/Error403";
+// import MainLayout from './layouts/MainLayout';
+// import Login from "./pages/auth/Login";
+// import Register from "./pages/auth/Register";
+// import Forgot from "./pages/auth/Forgot";  
+// import AuthLayout from "./layouts/AuthLayout";
+import React from 'react';
+import { Suspense } from 'react';
+import Loading from "./components/Loading";
 
 
 function App() {
+  const Dashboard = React.lazy(() => import("./pages/Dashboard"))
+  const Orders = React.lazy(() => import("./pages/Orders"))
+  const Customers = React.lazy(() => import("./pages/Customers"))
+  const NotFound = React.lazy(() => import("./pages/NotFound"))
+  const Error400 = React.lazy(() => import("./pages/Error400"))
+  const Error401 = React.lazy(() => import("./pages/Error401"))
+  const Error403 = React.lazy(() => import("./pages/Error403"))
+  const Login = React.lazy(() => import("./pages/auth/Login"))
+  const Register = React.lazy(() => import("./pages/auth/Register"))
+  const Forgot = React.lazy(() => import("./pages/auth/Forgot"))
+  const MainLayout = React.lazy(() => import("./layouts/MainLayout"))
+  const AuthLayout = React.lazy(() => import("./layouts/AuthLayout"))
+
   return (
-    <Routes>
-      <Route element={<MainLayout />}>
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route element={<MainLayout />}>
 
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/customers" element={<Customers />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/error400" element={<Error400 />} />
-        <Route path="/error401" element={<Error401 />} />
-        <Route path="/error403" element={<Error403 />} />
-        <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/error400" element={<Error400 />} />
+          <Route path="/error401" element={<Error401 />} />
+          <Route path="/error403" element={<Error403 />} />
+          <Route path="*" element={<NotFound />} />
 
 
-      </Route>
-       <Route element={<AuthLayout/>}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register/>} />
-            <Route path="/forgot" element={<Forgot/>} />
         </Route>
-    </Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot" element={<Forgot />} />
+        </Route>
+      </Routes>
+    </Suspense>
   )
 }
 
