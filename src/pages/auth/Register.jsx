da731fe390dd2aa2796ce1f419a5ee009861c24c
guest-persil <jihan24si@mahasiswa.pcr.ac.id>
2026-05-19 14:25:57 +0700

    penerapan component pada project sedap

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,67 +1,73 @@
+import React from 'react';
+// Pastikan jumlah titiknya dua (../../) karena file ini ada di dalam folder 'auth'
+import Container from "../../components/Container"; 
+
 export default function Register() {
     return (
-        <div>
-            <h2 className="text-2xl font-semibold text-gray-700 mb-6 text-center">
-                Create Your Account ✨
-            </h2>
+        <Container>
+            <div className="max-w-md mx-auto">
+                <h2 className="text-2xl font-semibold text-gray-700 mb-6 text-center">
+                    Create Your Account ✨
+                </h2>
 
-            <form>
-                <div className="mb-5">
-                    <label
-                        htmlFor="email"
-                        className="block text-sm font-medium text-gray-700 mb-1"
+                <form>
+                    <div className="mb-5">
+                        <label
+                            htmlFor="email"
+                            className="block text-sm font-medium text-gray-700 mb-1"
+                        >
+                            Email Address
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            className="w-full px-4 py-2 bg-gray-50 border border-gray-300 rounded-lg shadow-sm
+                                placeholder-gray-400 focus:ring-2 focus:ring-green-500 focus:outline-none"
+                            placeholder="you@example.com"
+                        />
+                    </div>
+
+                    <div className="mb-5">
+                        <label
+                            htmlFor="password"
+                            className="block text-sm font-medium text-gray-700 mb-1"
+                        >
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            className="w-full px-4 py-2 bg-gray-50 border border-gray-300 rounded-lg shadow-sm
+                                placeholder-gray-400 focus:ring-2 focus:ring-green-500 focus:outline-none"
+                            placeholder="********"
+                        />
+                    </div>
+
+                    <div className="mb-6">
+                        <label
+                            htmlFor="confirmPassword"
+                            className="block text-sm font-medium text-gray-700 mb-1"
+                        >
+                            Confirm Password
+                        </label>
+                        <input
+                            type="password"
+                            id="confirmPassword"
+                            className="w-full px-4 py-2 bg-gray-50 border border-gray-300 rounded-lg shadow-sm
+                                placeholder-gray-400 focus:ring-2 focus:ring-green-500 focus:outline-none"
+                            placeholder="********"
+                        />
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="w-full bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4
+                            rounded-lg transition duration-300 shadow-lg active:scale-95"
                     >
-                        Email Address
-                    </label>
-                    <input
-                        type="email"
-                        id="email"
-                        className="w-full px-4 py-2 bg-gray-50 border border-gray-300 rounded-lg shadow-sm
-                            placeholder-gray-400"
-                        placeholder="you@example.com"
-                    />
-                </div>
-
-                <div className="mb-5">
-                    <label
-                        htmlFor="password"
-                        className="block text-sm font-medium text-gray-700 mb-1"
-                    >
-                        Password
-                    </label>
-                    <input
-                        type="password"
-                        id="password"
-                        className="w-full px-4 py-2 bg-gray-50 border border-gray-300 rounded-lg shadow-sm
-                            placeholder-gray-400"
-                        placeholder="********"
-                    />
-                </div>
-
-                <div className="mb-6">
-                    <label
-                        htmlFor="confirmPassword"
-                        className="block text-sm font-medium text-gray-700 mb-1"
-                    >
-                        Confirm Password
-                    </label>
-                    <input
-                        type="password"
-                        id="confirmPassword"
-                        className="w-full px-4 py-2 bg-gray-50 border border-gray-300 rounded-lg shadow-sm
-                            placeholder-gray-400"
-                        placeholder="********"
-                    />
-                </div>
-
-                <button
-                    type="submit"
-                    className="w-full bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4
-                        rounded-lg transition duration-300"
-                >
-                    Register
-                </button>
-            </form>
-        </div>
-    )
+                        Register
+                    </button>
+                </form>
+            </div>
+        </Container>
+    );
 }
